343350c39712eb5da6bf224215d4fa6b65811f37
zjc-enigma <enigma@pku.edu.cn>
2017-03-03 14:38:13 +0800

    local

---
+++ bin/tf_svm.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn import datasets
+
+iris = datasets.load_iris()
+
+train_X = iris.data[:, :2]
+train_y = iris.target
+
+
+def cost():
+    pass
+
+
+
+def error():
+    pass
+
